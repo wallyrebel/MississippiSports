8a916d4f24960c765724c52c7e692d92564b0c35
Mississippi Sports <myersgrouponline@sportsmississippi.com>
2026-01-18 17:44:41 -0600

    Add 15 Belhaven University Blazers sports feeds
    - Baseball, M/W Basketball, Football, Men's Golf
    - M/W Soccer, Softball, M/W Tennis, M/W Track, M/W XC, Volleyball
    - Uses Sidearm Sports platform
    - Added blazers.belhaven.edu to trusted image hosts

--- src/rss_to_wp/images/rss_extractor.py
+++ src/rss_to_wp/images/rss_extractor.py
@@ -321,6 +321,7 @@ def is_valid_image_url(url: str) -> bool:
             "hailstate.com",  # Mississippi State athletics
             "olemisssports.com",  # Ole Miss athletics
             "gochoctaws.com",  # Mississippi College athletics
+            "blazers.belhaven.edu",  # Belhaven University athletics
         ]
         for host in known_image_hosts:
             if host in parsed.netloc.lower():
